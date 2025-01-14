2e9c83a1f9f907577a04812ea00cb70017b24340
Gehadazzam <gehadgamalazzam@gmail.com>
2025-01-14 15:50:25 +0200

    feat: use vueuse for Dark Mode

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
 import '@/assets/main.css'
+import { useDark } from '@vueuse/core'
+const isDark = useDark()
 </script>
 
 <template>
