4c7ca4597d88129fb9ca1e45a9ab4002a881fd33
Gehadazzam <gehadgamalazzam@gmail.com>
2025-01-14 15:41:53 +0200

    feat: remove keyCloack && use vueuse for Dark Mode

--- src/main.ts
+++ src/main.ts
@@ -6,24 +6,6 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import './assets/main.css'
-import keycloak from './keycloak'
-
-keycloak
-  .init({
-    onLoad: '',
-    checkLoginIframe: false,
-    promiseType: 'native',
-  })
-  .then(() => {
-    if (keycloak.authenticated) {
-      console.log('authenticated')
-    } else {
-      console.log('not authenticated')
-    }
-  })
-  .catch((err: string) => {
-    console.error(err)
-  })
 
 const app = createApp(App)
 
