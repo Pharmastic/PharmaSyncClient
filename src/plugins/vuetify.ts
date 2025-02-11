1f57fbf0c2cfa92107410f4f1b3f6462cfd3c455
Gehadazzam <gehadgamalazzam@gmail.com>
2025-02-11 22:43:00 +0200

    chore: add vuetify lib

---
+++ src/plugins/vuetify.ts
@@ -0,0 +1,9 @@
+// plugins/vuetify.js
+import { createVuetify } from 'vuetify';
+import * as components from 'vuetify/components'; // Import all Vuetify components
+import * as directives from 'vuetify/directives'; // Import all Vuetify directives
+
+export default createVuetify({
+  components, // Register all Vuetify components
+  directives, // Register all Vuetify directives
+});
